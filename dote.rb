c598c27ceaa40307d69e43fcaa1311e364e2118b
KiitoTakase <kmc2527@kamiyama.ac.jp>
2025-10-28 11:17:57 +0900

    Update dote.rb

--- dote.rb
+++ dote.rb
@@ -3,7 +3,7 @@ class Dote < Formula
   homepage "https://github.com/KiitoTakase/homebrew-dote"
   url "https://raw.githubusercontent.com/KiitoTakase/homebrew-dote/main/dote.c"
   sha256 "03ea4c392aa6e2d712d0c26f4b61e0bd8b50e45a9bf08ffd55e9c02d85260d11"
-  version “1.0.0”
+  version "1.0.0"
 
   def install
     system ENV.cc, "dote.c", "-o", "dote"
