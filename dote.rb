4c83165c69fe478f21ae271062b299ab59ece5d3
KiitoTakase <kmc2527@kamiyama.ac.jp>
2025-10-28 11:13:23 +0900

    Update dote.rb

--- dote.rb
+++ dote.rb
@@ -2,7 +2,7 @@ class Dote < Formula
   desc "dot e command"
   homepage "https://github.com/KiitoTakase/homebrew-dote"
   url "https://raw.githubusercontent.com/KiitoTakase/homebrew-dote/main/dote.c"
-  sha256 "PUT_SHA256_OF_DOTE.C_HERE"
+  sha256 "03ea4c392aa6e2d712d0c26f4b61e0bd8b50e45a9bf08ffd55e9c02d85260d11"
 
   def install
     system ENV.cc, "dote.c", "-o", "dote"
